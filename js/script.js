d11e1c75afacad3645b5b0d462285c15de8bd5e9
Thommie Wallin <thommie.wallin@chasacademy.se>
2020-11-08 21:00:39 +0100

    add function createLetterBox()

--- js/script.js
+++ js/script.js
@@ -10,11 +10,19 @@ let guesses = 0;     // Number: håller antalet gissningar som gjorts
 let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 
 let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
+
+// DOM-nod: knappen som du startar spelet med
+let startGameBtnEl = document.querySelector('#startGameBtn');
+startGameBtnEl.addEventListener('click', startGame);
 let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
 let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
+
+function startGame() {
+  selectedWord = randomWord(wordList);
+  createLetterBoxes(selectedWord);
+}
 
 // ----------------------------------------------------------------------
 // Funktion som slumpar fram ett ord
@@ -27,6 +35,31 @@ function randomWord(arr) {
 // ----------------------------------------------------------------------
 
 // Funktion som tar fram bokstävernas rutor, antal rutor beror på vilket ord slumptas fram
+
+function createLetterBoxes(word) {
+  let letters = word.split('');
+  const letterBoxEls = document.querySelector('#letterBoxes > ul');  
+  const removeEl = document.querySelectorAll('#letterBoxes li');
+  const newLi = document.createElement('li');
+
+  // Remove old word
+  for (let i = 0; i < removeEl.length; i++) {
+    letterBoxEls.removeChild(removeEl[i]);    
+  }  
+  
+  // Set a new word
+  for (let i = 0; i < letters.length; i++) {
+        
+    const newInput = document.createElement('input');
+    newInput.setAttribute('type', 'text');
+    newInput.setAttribute('disabled', 'true');
+    newInput.setAttribute('value', letters[i]);
+    
+    newLi.appendChild(newInput);
+    letterBoxEls.appendChild(newLi);
+  }
+}
+
 // Funktion som körs när du trycker på bokstäverna och gissar bokstav
 // Funktion som ropas vid vinst eller förlust, gör olika saker beroende tillståndet
 // Funktion som inaktiverar/aktiverar bokstavsknapparna beroende på vilken del av spelet du är på
